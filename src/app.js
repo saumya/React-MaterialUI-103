f6da7ebb0a4cdef9a0bde13515909c68e7158d25
saumya <2saumyaray@gmail.com>
2016-04-16 15:38:26 +0530

    wip, application initiated

--- src/app.js
+++ src/app.js
@@ -1,4 +1,16 @@
 /*
  * Application Entry
  */
- (function(){})();
+ (function(){
+   //
+   var React = require('react');
+   var ReactDOM = require('react-dom');
+   //
+   var Router = require('react-router').Router;
+   var Route = require('react-router').Route;
+   var IndexRoute = require('react-router').IndexRoute;
+   var hashHistory = require('react-router').hashHistory;
+   //
+
+   var App = ReactDOM.render(<div>React!</div>,document.getElementById('react-app'));
+ })();
