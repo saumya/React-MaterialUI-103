f519c24a095a427983b38b641f7c9343c1e0a2b2
saumya <2saumyaray@gmail.com>
2016-04-16 19:23:29 +0530

    Button rendering passed

--- src/component/AppControllerView.react.js
+++ src/component/AppControllerView.react.js
@@ -1,6 +1,6 @@
 /*
  * Application Entry
- *
+ * mb1 : mui-0.15.0-beta-1
  */
 var React = require ('react');
 // react-router
@@ -12,29 +12,33 @@ var browserHistory = require('react-router').browserHistory;
 var AppBar = require('material-ui/AppBar') ;
 //import AppBar from 'material-ui/AppBar';
 import FlatButton from 'material-ui/FlatButton';
-
+import RaisedButton from 'material-ui/RaisedButton';
+// changes for material-ui 0.15.0-beta-1
 import baseTheme from 'material-ui/styles/baseThemes/darkBaseTheme';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
 
-//
+// Component declaration
 var AppControllerView = React.createClass({
   contextTypes: {
     router: React.PropTypes.object.isRequired,
   },
-
+  // changes for material-ui 0.15.0-beta-1
   childContextTypes:{
     muiTheme: React.PropTypes.object.isRequired,
   },
   getChildContext() {
     return {muiTheme: getMuiTheme(baseTheme)};
   },
+  // changes for material-ui 0.15.0-beta-1
 
   render: function(){
     var that = this;
     return(
       <div>
         <div>Application View</div>
-        <FlatButton label="Default" />
+        <RaisedButton label="Secondary" secondary={true} />
+        <FlatButton label="FlatButton" />
+        <RaisedButton label="Primary" primary={true} />
         { /* renders the children */ this.props.children }
       </div>
     );
